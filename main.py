4bce21d8709f9f154052ee21f4b9f2e39a1a4e21
Imaspson <maier.samuel99@gmail.com>
2023-04-29 18:10:23 +0300

    Add threading to clean and send email

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import glob
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -54,8 +55,13 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        clean_thread.start()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
